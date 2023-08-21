1f2d4c4dc7317a33e4852b2ffeb4bc904f6db58f
erik465 <ehalasz06@gmail.com>
2023-08-21 11:23:24 +0200

    .

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { StyledButton } from './Button.styled'
 
 const Button = ({loadMore}) => {
   return (
-    <button onClick={loadMore}>Load more</button>
+    <StyledButton onClick={loadMore}>Load more</StyledButton>
   )
 }
 
